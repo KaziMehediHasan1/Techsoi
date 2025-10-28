2358b5356c2e1f9e0396cfa9048e8e16fa3818f5
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-10-28 11:23:40 +0600

    add mininavbar

--- app/components/layouts/MiniNavbar.tsx
+++ app/components/layouts/MiniNavbar.tsx
@@ -8,22 +8,23 @@ const MiniNavbar = ({ name, offer }: { name: string; offer: string }) => {
     <CommonWrapper>
       <section className="flex items-center justify-between">
         <div className="cursor-pointer flex items-center gap-x-2">
-          <Image src={CallIcon} alt="My Icon" width={15} height={15} />
-          <p className="leading-[150%] text-sm">Phone +888 8888 888</p>
+          <Image src={CallIcon} alt="My Icon" className="w-2 sm:w-4" />
+          <p className="leading-[150%] text-[8px] sm:text-sm">Phone +888 8888 888</p>
         </div>
 
-        <div className="leading-[150%] text-sm">
+        <div className="leading-[150%] text-[8px] sm:text-sm">
           {offer
             ? offer
             : "Get Upto 25% cashback using this Coupon Code: 8558212"}
         </div>
 
-        <div className="cursor-pointer flex items-center gap-x-2">
-          <Image src={UserIcon} alt="My Icon" width={15} height={15} />
+        <div className="cursor-pointer hidden sm:flex items-center gap-x-2">
+          <Image src={UserIcon} alt="My Icon"  className="w-4 h-4" />
           <p className="leading-[150%] text-sm">
             {name ? name : "Mr. Jhon Don"}
           </p>
         </div>
+
       </section>
     </CommonWrapper>
   );
