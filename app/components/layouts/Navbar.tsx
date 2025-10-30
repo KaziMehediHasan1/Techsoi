f90912220e1bf203bb8e87af197287972a444041
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-10-30 19:21:33 +0600

    almost completed serarch modal

--- app/components/layouts/Navbar.tsx
+++ app/components/layouts/Navbar.tsx
@@ -6,11 +6,13 @@ import SearchIcon from "@/public/icons/search.svg";
 import CommonWrapper from "./CommonWrapper";
 import Image from "next/image";
 import Link from "next/link";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Debounce } from "@/app/lib/utils/Debounce";
-import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
+import { HiOutlineMenu } from "react-icons/hi";
+import ModalForSearchBar from "../shared/ModalForSearchBar";
 
 const Navbar = () => {
+  const [modal, setModal] = useState<boolean>(false);
   const debouncedSearch = useMemo(
     () =>
       Debounce((text: string) => {
@@ -18,40 +20,47 @@ const Navbar = () => {
       }, 2000),
     []
   );
+
+  const openModal = () => setModal(true);
+  const closeModal = () => setModal(false);
+  console.log(modal, "madal er ki khobor");
   return (
-    <CommonWrapper>
-      <section className="flex items-center justify-between">
-        {/* THIS SECTION FOR MOBILE DEVICE ONLY */}
+    <>
+      <CommonWrapper>
+        <section className="flex items-center justify-between">
+          {/* THIS SECTION FOR MOBILE DEVICE ONLY - Start */}
 
-        <section className="flex items-center gap-3">
-          <div className="block md:hidden">
-            <HiOutlineMenu size="25" />
-          </div>
-          <Link href="/">
-            <Image
-              src={Logo}
-              alt="My Icon"
-              className="w-[120px] md:w-[200px]"
+          <section className="flex items-center gap-3">
+            <div className="block md:hidden">
+              <HiOutlineMenu size="25" />
+            </div>
+            <Link href="/">
+              <Image
+                src={Logo}
+                alt="My Icon"
+                className="w-[120px] md:w-[200px]"
+              />
+            </Link>
+          </section>
+
+          {/* THIS SECTION FOR MOBILE DEVICE ONLY - End */}
+
+          <div className="relative hidden md:block w-full max-w-xl">
+            <input
+              onChange={(e) => {
+                debouncedSearch(e.target.value);
+              }}
+              type="search"
+              placeholder="Search products.."
+              className="placeholder:text-basicDark text-basicDark border-primary-100 border py-2 rounded-xl focus:outline-none px-4 w-full my-input"
             />
-          </Link>
-        </section>
+            <button type="submit" className="absolute inset-y-0 right-0 pr-3">
+              <Image src={SearchIcon} alt="Favourite" width={20} height={20} />
+            </button>
+          </div>
 
-        <div className="relative hidden lg:block w-full max-w-xl">
-          <input
-            onChange={(e) => {
-              debouncedSearch(e.target.value);
-            }}
-            type="search"
-            placeholder="Search products.."
-            className="placeholder:text-basicDark text-basicDark border-primary-100 border py-2 rounded-xl focus:outline-none px-4 w-full my-input"
-          />
-          <button type="submit" className="absolute inset-y-0 right-0 pr-3">
-            <Image src={SearchIcon} alt="Favourite" width={20} height={20} />
-          </button>
-        </div>
-
-        <div className="flex items-center gap-1 sm:gap-2 md:gap-5">
-          <div className="block md:hidden w-10 h-10">
+          <div className="flex items-center gap-1 sm:gap-2 md:gap-5">
+            <div onClick={openModal} className="block md:hidden w-10 h-10">
               <button type="submit">
                 <Image
                   src={SearchIcon}
@@ -60,47 +69,52 @@ const Navbar = () => {
                 />
               </button>
             </div>
-          <section className="flex items-center gap-2">
-            
-            <div className="relative w-10 h-10">
-              <Image
-                src={Favourite}
-                alt="Favourite"
-                className="w-full h-full bg-primary-50 p-2 rounded-full"
-              />
-              <span className="absolute inset-y-1 right-0.5 flex items-center justify-center w-4.5 h-4.5 text-[10px] leading-[150%] text-white bg-primary-500 rounded-full">
-                4
-              </span>
-            </div>
-            <div className="hidden md:block">
-              <p className="text-sm md:text-[18px] text-primary-500 font-medium">
-                Favourite
-              </p>
-              <p className="text-xs md:text-sm">৳260</p>
-            </div>
-          </section>
+            <section className="flex items-center gap-2">
+              <div className="relative w-10 h-10">
+                <Image
+                  src={Favourite}
+                  alt="Favourite"
+                  className="w-full h-full bg-primary-50 p-2 rounded-full"
+                />
+                <span className="absolute inset-y-1 right-0.5 flex items-center justify-center w-4.5 h-4.5 text-[10px] leading-[150%] text-white bg-primary-500 rounded-full">
+                  4
+                </span>
+              </div>
+              <div className="hidden md:block">
+                <p className="text-sm md:text-[18px] text-primary-500 font-medium">
+                  Favourite
+                </p>
+                <p className="text-xs md:text-sm">৳260</p>
+              </div>
+            </section>
 
-          <section className="flex items-center gap-2">
-            <div className="relative w-10 h-10">
-              <Image
-                src={Shopping}
-                alt="Shopping"
-                className="w-full h-full bg-primary-50 p-2 rounded-full"
-              />
-              <span className="absolute inset-y-1 right-0.5 flex items-center justify-center w-4.5 h-4.5 text-[10px] leading-[150%] text-white bg-primary-500 rounded-full">
-                9
-              </span>
-            </div>
-            <div className="hidden md:block">
-              <p className="text-sm md:text-[18px] text-primary-500 font-medium">
-                My Cart
-              </p>
-              <p className="text-xs md:text-sm">৳590</p>
-            </div>
-          </section>
-        </div>
+            <section className="flex items-center gap-2">
+              <div className="relative w-10 h-10">
+                <Image
+                  src={Shopping}
+                  alt="Shopping"
+                  className="w-full h-full bg-primary-50 p-2 rounded-full"
+                />
+                <span className="absolute inset-y-1 right-0.5 flex items-center justify-center w-4.5 h-4.5 text-[10px] leading-[150%] text-white bg-primary-500 rounded-full">
+                  9
+                </span>
+              </div>
+              <div className="hidden md:block">
+                <p className="text-sm md:text-[18px] text-primary-500 font-medium">
+                  My Cart
+                </p>
+                <p className="text-xs md:text-sm">৳590</p>
+              </div>
+            </section>
+          </div>
+        </section>
+      </CommonWrapper>
+
+      {/* when click the search icon then modal is open */}
+      <section className="flex items-center justify-center">
+        {modal && <ModalForSearchBar closeModal={closeModal} />}
       </section>
-    </CommonWrapper>
+    </>
   );
 };
 
--- app/components/shared/ModalForSearchBar.tsx
+++ app/components/shared/ModalForSearchBar.tsx
@@ -1,7 +1,47 @@
-const ModalForSearchBar = () => {
-  return <div>
-    
-  </div>;
+import SearchIcon from "@/public/icons/search.svg";
+import { Debounce } from "@/app/lib/utils/Debounce";
+import Image from "next/image";
+import { useMemo } from "react";
+import { HiOutlineX } from "react-icons/hi";
+
+const ModalForSearchBar = ({ closeModal }: { closeModal: () => void }) => {
+  const debouncedSearch = useMemo(
+    () =>
+      Debounce((text: string) => {
+        console.log("Searching for:", text);
+      }, 2000),
+    []
+  );
+  return (
+    <section className="fixed inset-0 md:hidden flex justify-center top-1/5 sm:top-1/4 z-50">
+      <div className="relative bg-primary-300/50 backdrop-blur-lg w-full max-w-[90%] h-1/2 py-8 px-4 transform translate-all rounded-xl shadow-lg duration-300">
+        <div className="relative md:hidden block w-full mt-5">
+          <input
+            onChange={(e) => {
+              debouncedSearch(e.target.value);
+            }}
+            type="search"
+            placeholder="Search products.."
+            className="placeholder:text-basicDark text-black border-white border py-2 rounded-xl focus:outline-none px-4 w-full my-input"
+          />
+          <button type="submit" className="absolute inset-y-0 right-0 pr-3 ">
+            <Image src={SearchIcon} alt="Favourite" width={20} height={20} />
+          </button>
+        </div>
+
+        {/* SEARCH DATA SHOWING THIS PLACE - START */}
+        <div className="my-5 text-basicDarkOne text-center">Search your favourite products...</div>
+        {/* SEARCH DATA SHOWING THIS PLACE - END */}
+
+        <button
+          onClick={closeModal}
+          className="absolute top-3 right-4 text-2xl font-bold text-gray-700 hover:text-black"
+        >
+          <HiOutlineX />
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default ModalForSearchBar;
