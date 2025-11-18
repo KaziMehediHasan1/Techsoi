13d15e8b1a5cca53cc4ba465ddaadda7b105d23c
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-18 09:42:44 +0600

    Topsellingproducts section added

---
+++ app/components/Home/TopSellingProducts.tsx
@@ -0,0 +1,91 @@
+"use client";
+import Image from "next/image";
+import CommonWrapper from "../layouts/CommonWrapper";
+import PrimaryTitle from "../shared/PrimaryTitle";
+import Favourite from "@/public/icons/favourite.svg";
+import Shopping from "@/public/icons/shopping-cart.svg";
+import Buy from "@/public/icons/shopping-basket-01.svg";
+import Star from "@/public/icons/Star.svg";
+import cards from "@/app/lib/constants/CardsData";
+const TopSellingProducts = () => {
+  return (
+    <CommonWrapper>
+      <div className="space-y-12 mt-20">
+        <PrimaryTitle
+          headingOne="Top Selling"
+          headingTwo=" Products"
+          subheading="Get Your Desired Product from Featured Category!"
+        />
+        {/* CARDS DATA START */}
+        <div className="grid gap-6 sm:grid-cols-3 lg:grid-cols-4 justify-items-center">
+          {cards.map((item) => (
+            <section
+              key={item.id}
+              className="border border-primary-100 rounded-[20px] space-y-4  p-4 w-full max-w-[360px]"
+            >
+              <Image
+                alt={item.title}
+                src={item.image}
+                width={200}
+                height={200}
+                className="w-fit mx-auto"
+              />
+              <h1 className="text-[22px] font-medium leading-[130%] text-basicDarkTwo">
+                {item.title}
+              </h1>
+              <section className="flex items-center justify-between">
+                <div className="flex items-center gap-x-3">
+                  <p className="text-2xl font-semibold leading-[130%] text-primary-500">
+                    {item.price}
+                  </p>
+                  <del className="text-[16px] leading-[150%] font-normal text-basicDark ">
+                    {item.previousprice}
+                  </del>
+                </div>
+                <div className="flex items-center ">
+                  <Image
+                    src={Star}
+                    alt="Favourite"
+                    className="w-8 h-8 p-2 rounded-full"
+                  />
+                  <p>
+                    {item.review} ({item.totalreviews})
+                  </p>
+                </div>
+              </section>
+              <section className="flex items-center justify-between">
+                <button className="flex items-center gap-x-3 cursor-pointer bg-primary-50 pl-2 pr-4 py-1 rounded-xl">
+                  <Image
+                    src={Buy}
+                    alt="Favourite"
+                    className="w-10 h-10  p-2 rounded-full"
+                  />
+                  <p>Buy Now</p>
+                </button>
+                <div className="flex items-center gap-x-3">
+                  <button className="cursor-pointer">
+                    <Image
+                      src={Shopping}
+                      alt="Favourite"
+                      className="w-full h-full bg-primary-50  p-3 rounded-xl"
+                    />
+                  </button>
+                  <button className="cursor-pointer">
+                    <Image
+                      src={Favourite}
+                      alt="Favourite"
+                      className="w-full h-full bg-primary-50 p-3 rounded-xl"
+                    />
+                  </button>
+                </div>
+              </section>
+            </section>
+          ))}
+        </div>
+        {/* CARDS DATA END */}
+      </div>
+    </CommonWrapper>
+  );
+};
+
+export default TopSellingProducts;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryProducts from "./components/Home/CategoryProducts";
 import FeaturedProducts from "./components/Home/FeaturedProducts";
 import Hero from "./components/Home/Hero";
+import TopSellingProducts from "./components/Home/TopSellingProducts";
 import CommonWrapper from "./components/layouts/CommonWrapper";
 
 export default async function Home() {
@@ -9,6 +10,7 @@ export default async function Home() {
       <Hero />
       <CategoryProducts />
       <FeaturedProducts />
+      <TopSellingProducts/>
     </CommonWrapper>
   );
 }
